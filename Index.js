26689c35709374701e7a400c11820620e940cd2a
Olivia Johnson <oliviajohnson@Olivias-MacBook-Air.local>
2023-09-06 19:55:03 +0100

    prgrss update

--- Index.js
+++ Index.js
@@ -1,5 +1,14 @@
 let count = 0
+let countEl = document.getElementById("count-el")
+let prevEl = document.getElementById("prev-ent")
 
 function increment(){
-    console.log
+    count = count + 1;
+    countEl.innerText = count;
 }
+
+function save() {
+    prevEl.innerText = prevEl.innerText + "-" + count;
+}
+
+
