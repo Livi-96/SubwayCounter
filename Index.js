6c258846802e5f0929cf7bbbde028b7f6b4c8b58
Olivia Johnson <oliviajohnson@Olivias-Air.lan>
2023-09-07 08:12:35 +0100

    completed app

--- Index.js
+++ Index.js
@@ -2,13 +2,17 @@ let count = 0
 let countEl = document.getElementById("count-el")
 let prevEl = document.getElementById("prev-ent")
 
+
+
 function increment(){
     count = count + 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 
 function save() {
-    prevEl.innerText = prevEl.innerText + "-" + count;
+    prevEl.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = count
 }
 
 
